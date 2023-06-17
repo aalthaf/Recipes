f88c3028a52c5554fe9ad5a7be867e693d08b4d6
Althaf Ahamed <althafahmed68@hotmail.com>
2023-06-16 22:17:52 -0400

    formatting

--- public/js/script.js
+++ public/js/script.js
@@ -9,13 +9,13 @@ xhrGET.setRequestHeader("Content-Type", "application/json")
 
 xhrGET.onreadystatechange = () => {
     console.log(JSON.parse(xhrGET.responseText))
-    if (xhrGET.readyState == 4 && xhrGET.readyState==200) {
+    if (xhrGET.readyState == 4 && xhrGET.readyState == 200) {
         let ingredientDiv = document.getElementById('ingredientDIV')
         ingredientDiv.innerHTML = ''
         let response = JSON.parse(xhrGET.responseText)
 
         for (var key in response.recipes)
-        ingredientDiv.innerHTML = ingredientDiv.innerHTML + `
+            ingredientDiv.innerHTML = ingredientDiv.innerHTML + `
         <figure>
         <a href="${response.recipes[key].source_url}">
         <img src = "${response.recipes[key].image_url}" width="410" height="340">
@@ -32,23 +32,22 @@ xhrGET.onreadystatechange = () => {
 function getRecipe() {
     // uses post request
     let ingredientName = document.getElementById('ingredient').value
-    if(ingredientName === '') {
+    if (ingredientName === '') {
         return alert('Please enter an ingredient')
     }
 
     let url = '/recipes'
     let ingredient_data = {
-      ingredient : ingredientName
+        ingredient: ingredientName
     }
     let ingredientDiv = document.getElementById('ingredientDIV')
     ingredientDiv.innerHTML = ''
-	//console.log("hello");
     let xhr = new XMLHttpRequest()
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
             let response = JSON.parse(xhr.responseText)
-      for (var key in response.recipes)
- 			ingredientDiv.innerHTML = ingredientDiv.innerHTML + `
+            for (var key in response.recipes)
+                ingredientDiv.innerHTML = ingredientDiv.innerHTML + `
 
       <figure>
       <a href="${response.recipes[key].source_url}">
@@ -60,19 +59,19 @@ function getRecipe() {
         }
     }
 
-      xhr.open('POST', url, true)
-      xhr.setRequestHeader("Content-Type", "application/json")
-      xhr.send(JSON.stringify(ingredient_data))
+    xhr.open('POST', url, true)
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send(JSON.stringify(ingredient_data))
 }
 
 
 
 //Attach Enter-key Handler
-const ENTER=13
+const ENTER = 13
 document.getElementById("ingredient")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === ENTER) {
-        document.getElementById("submit").click();
-    }
-});
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode === ENTER) {
+            document.getElementById("submit").click();
+        }
+    });
--- server.js
+++ server.js
@@ -11,13 +11,13 @@ let API_KEY = '3811628764de732526a001fe10ce5fa3' //API KEY
 
 const app = express()
 
-function myLogger(request,response, next){
+function myLogger(request, response, next) {
   console.log(`METHOD: ${request.method}`)
   console.log(`URL: ${request.url}`)
   console.log(`PATH: ${request.path}`)
   next() //allow next middleware to run
 }
-function messageSeparator(request,response, next){
+function messageSeparator(request, response, next) {
   console.log(`---------------------------------------------------------`)
   next() //allow next middleware to run
 }
@@ -29,29 +29,29 @@ app.use(express.static(__dirname + '/public')) //static server
 app.use(bodyParser.json()) //parse JSON in POST bodies.
 
 //Routes
-app.get(['/','/index.html','/recipes.html','/recipes'], (request, response) => {
+app.get(['/', '/index.html', '/recipes.html', '/recipes'], (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 
 })
 
-app.get('/recipes',(request,response) =>{
+app.get('/recipes', (request, response) => {
 
   //using queries
   let ingredient = request.query.ingredient
 
-    let url = ''
-    if(!ingredient) {
-      return response.json({message: 'Please enter an ingredient name'})
-      url = `https://food2fork.com/api/search?key=${API_KEY}`
-    }
-    else {
-      url = `https://food2fork.com/api/search?key=${API_KEY}&q=${ingredient}`
-    }
-    http_request.get(url, (err, res, data) => {
-      return response.contentType('application/json').json(JSON.parse(data))
+  let url = ''
+  if (!ingredient) {
+    return response.json({ message: 'Please enter an ingredient name' })
+    url = `https://food2fork.com/api/search?key=${API_KEY}`
+  }
+  else {
+    url = `https://food2fork.com/api/search?key=${API_KEY}&q=${ingredient}`
+  }
+  http_request.get(url, (err, res, data) => {
+    return response.contentType('application/json').json(JSON.parse(data))
 
 
-    })
+  })
 
 })
 
@@ -62,8 +62,8 @@ app.post('/recipes', (request, response) => {
 
 
   let url = ''
-  if(!ingredient) {
-    return response.json({message: 'Please enter an ingredient name'})
+  if (!ingredient) {
+    return response.json({ message: 'Please enter an ingredient name' })
     url = `https://food2fork.com/api/search?key=${API_KEY}`
   }
   else {
@@ -79,13 +79,13 @@ app.post('/recipes', (request, response) => {
 
 //start server
 app.listen(PORT, err => {
-  if(err) console.log(err)
+  if (err) console.log(err)
   else {
     console.log(`Server listening on port: ${PORT}`)
     console.log(`To Test:`)
     console.log('http://localhost:3000/recipes.html')
     console.log('http://localhost:3000/recipes')
-      console.log('http://localhost:3000/')
+    console.log('http://localhost:3000/')
     console.log(`http://localhost:3000/recipes?ingredient=Basil`)
     console.log(`http://localhost:3000`)
   }
